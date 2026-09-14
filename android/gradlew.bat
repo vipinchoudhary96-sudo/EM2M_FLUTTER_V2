327f4dcfaedb5baf2b2641fe1e78ddfe7125a1a0
vipinchoudhary96-sudo <vipinchoudhary96@gmail.com>
2026-09-14 18:30:27 +0530

    Update gradlew.bat

--- android/gradlew.bat
+++ android/gradlew.bat
@@ -1,1 +1,16 @@
+@rem
+@rem Copyright 2015 the original author or authors.
+@rem
+@rem Licensed under the Apache License, Version 2.0.
+@rem
 
+@echo off
+
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+
+set APP_HOME=%DIRNAME%
+
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+
+"%JAVA_HOME%\bin\java.exe" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
